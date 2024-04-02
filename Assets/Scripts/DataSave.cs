c657bce0c0000f429ac4f0cc4cbe5e07678a36f5
harunesad <harun.esad@gmail.com>
2024-04-02 17:59:11 +0300

    dotween added and uı fixed

---
+++ Assets/Scripts/DataSave.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataSave : MonoBehaviour
+{
+    private static DataSave _instance;
+
+    public static DataSave Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                GameObject singletonObject = new GameObject("SingletonExample");
+                _instance = singletonObject.AddComponent<DataSave>();
+            }
+
+            return _instance;
+        }
+    }
+    public float effectSound, mainSound;
+    public int characterIndex;
+    public float coinCount;
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    public void SoundUpdate(float effectSound, float mainSound)
+    {
+        if (effectSound >= 0)
+        {
+            this.effectSound = effectSound;
+        }
+        if (mainSound >= 0)
+        {
+            this.mainSound = mainSound;
+        }
+    }
+    public void CharacterSelect(int index)
+    {
+        characterIndex = index;
+    }
+}
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -3,27 +3,88 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
-using System.Linq;
-using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 public class UIManager : MonoBehaviour
 {
-    public Button play, settings, character;
-    public Button leave, home, playGame;
-    public TextMeshProUGUI killInfo, deathInfo, killCountText, win, time, coin;
-    public GameObject killImage, damagePopup, settingsPanel;
-    public Image gameBeforeLoading, gameLoading;
-    [SerializeField] GameObject canvas;
-    [SerializeField] Image gameBackground, charactersBackground, settingsBackgorund;
-    public Image waitBackground;
-    [SerializeField] AudioClip click, loadingSound;
-    public AudioSource chooseSource, loadingSource, birdSource;
-    [SerializeField] Slider effectsound, mainSound;
-    public Sprite winSprite, loseSprite;
+    [SerializeField] Button play, settings, characterChoose;
+    [SerializeField] Button home, mainHome;
+    [SerializeField] CanvasGroup characterWindow, settingsWindow, entryWindow;
+    [SerializeField] Sprite select, unselect;
+    [SerializeField] List<Button> characters;
+    Image choosen;
     void Start()
     {
-
+        mainHome.onClick.AddListener(MainHome);
+        characterChoose.onClick.AddListener(CharacterWindow);
+        settings.onClick.AddListener(SettingsOpen);
+        play.onClick.AddListener(PlayGame);
+        home.onClick.AddListener(HomeOpen);
+        for (int i = 0; i < characters.Count; i++)
+        {
+            int j = i;
+            characters[i].onClick.AddListener(delegate { CharacterSelect(characters[j].transform.parent.GetComponent<Image>(), j); });
+        }
+    }
+    void MainHome()
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+    void CharacterWindow()
+    {
+        UITransition(entryWindow, characterWindow);
+    }
+    void SettingsOpen()
+    {
+        UITransition(entryWindow, settingsWindow);
+        entryWindow.blocksRaycasts = false;
+    }
+    void CharacterSelect(Image choosenImage, int index)
+    {
+        if (choosen != null && choosen != choosenImage)
+        {
+            choosen.sprite = unselect;
+            choosen = choosenImage;
+            choosen.sprite = select;
+        }
+        else if (choosen == null)
+        {
+            choosen = choosenImage;
+            choosen.sprite = select;
+        }
+        DataSave.Instance.CharacterSelect(index);
+    }
+    void PlayGame()
+    {
+        characterWindow.blocksRaycasts = false;
+        characterWindow.DOFade(0, 1).SetEase(Ease.Linear).OnComplete(() =>
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        });
+    }
+    public void MainSoundUpdate(Slider slider)
+    {
+        DataSave.Instance.SoundUpdate(-1, slider.value);
+    }
+    public void EffectSoundUpdate(Slider slider)
+    {
+        DataSave.Instance.SoundUpdate(slider.value, -1);
+    }
+    void HomeOpen()
+    {
+        UITransition(settingsWindow, entryWindow);
+    }
+    void UITransition(CanvasGroup close, CanvasGroup open)
+    {
+        close.blocksRaycasts = false;
+        close.DOFade(0, 1).SetEase(Ease.Linear).OnComplete(() =>
+        {
+            open.DOFade(1, 1).SetEase(Ease.Linear).OnComplete(() =>
+            {
+                open.blocksRaycasts = true;
+            });
+        });
     }
 }
-    
-    
