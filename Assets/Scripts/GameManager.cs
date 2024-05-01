0b73a218670e104578a708c9ed9408648c13ced6
harunesad <harun.esad@gmail.com>
2024-05-01 17:44:46 +0300

    player fixed

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -23,7 +23,7 @@ public class GameManager : MonoBehaviour
         enemyList.Add(enemies.GetChild(0));
         enemyList[0].GetComponent<NavMeshAgent>().SetDestination(player.GetComponent<PlayerControl>().points[0].position);
         enemyList[0].GetComponent<Animator>().SetBool("Walk", true);
-        InvokeRepeating("Builder", 0, 1);
+        //InvokeRepeating("Builder", 0, 1);
         //surface.BuildNavMesh();
     }
     void Builder()
@@ -36,11 +36,14 @@ public class GameManager : MonoBehaviour
     {
         for (int i = 0; i < enemyList.Count; i++)
         {
-            bool posX = Mathf.Approximately(player.GetComponent<PlayerControl>().points[i].position.x, enemyList[i].position.x);
-            bool posZ = Mathf.Approximately(player.GetComponent<PlayerControl>().points[i].position.z, enemyList[i].position.z);
+            //bool posX = Mathf.Approximately(player.GetComponent<PlayerControl>().points[i].position.x, enemyList[i].position.x);
+            //bool posZ = Mathf.Approximately(player.GetComponent<PlayerControl>().points[i].position.z, enemyList[i].position.z);
+            bool posX = (Mathf.Abs(player.GetComponent<PlayerControl>().points[i].position.x - enemyList[i].position.x) <= .01f);
+            bool posZ = (Mathf.Abs(player.GetComponent<PlayerControl>().points[i].position.z - enemyList[i].position.z) <= .01f);
+            enemyList[i].transform.LookAt(player.position);
             if (!enemyList[i].GetComponent<NavMeshAgent>().hasPath)
             {
-                enemyList[i].transform.LookAt(player.position);
+                //enemyList[i].transform.LookAt(player.position);
                 enemyList[i].GetComponent<Animator>().SetBool("Walk", false);
                 if ((!posX || !posZ) && enemyList[i].GetComponentInChildren<CanvasGroup>().alpha != 1)
                 {
